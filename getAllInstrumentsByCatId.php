21737f7cd785f12c4c570d939f4b78cabb8c2ae9
HazielDev <hazieldev@gmail.com>
2024-12-10 13:03:40 -0600

    Se sube conseguir instrumentos por catid

---
+++ getAllInstrumentsByCatId.php
@@ -0,0 +1,26 @@
+<?php 
+
+include 'default.php';
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {die("Error al conectar a la base de datos: " . mysqli_connect_error());}
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $catID = $_GET['catId'];
+    $getCats = $conn->prepare("SELECT * FROM Instrumento WHERE catId = ?");
+    $getCats->bind_param("i",$catID);
+    if($getCats->execute()){
+        $catList = [];
+        $catsRes = $getCats->get_result(); 
+        if($catsRes->num_rows > 0){
+            while ($cat = $catsRes->fetch_assoc()) {
+                $catList[] = $cat;
+            }
+            echo json_encode($catList);
+        }else{
+            echo json_encode(['message' => 'No se encontro ningun instrumento']);
+        }
+    }
+}
+
+
+?>
