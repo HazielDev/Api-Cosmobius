32593d863e27776fba82b28252dd371bd96bbe98
HazielDev <hazieldev@gmail.com>
2024-12-09 23:24:43 -0600

    Que pasa?

--- getAllInstruments.php
+++ getAllInstruments.php
@@ -1,11 +1,15 @@
 <?php 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     require_once 'default.php';
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {die("Error al conectar a la base de datos: " . mysqli_connect_error());}
     
-    header("Content-Type: application/json");
+
     $guitars = [];
     $allInstrumento = $conn->prepare("SELECT * FROM Instrumento");
     if($allInstrumento->execute()){
