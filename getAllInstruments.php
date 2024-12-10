d2d8c58f5c2eb20f4520d1bf60ad02867891bbf8
HazielDev <hazieldev@gmail.com>
2024-12-09 23:26:42 -0600

    Correccion de los instrumentos

--- getAllInstruments.php
+++ getAllInstruments.php
@@ -9,6 +9,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {die("Error al conectar a la base de datos: " . mysqli_connect_error());}
     
+    header("Content-Type: application/json");
 
     $instrumentos = [];
     $allInstrumento = $conn->prepare("SELECT * FROM Instrumento");
