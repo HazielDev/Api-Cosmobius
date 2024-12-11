9bdafd60b80dec76de66c4b62dd298b775bcfb51
HazielDev <hazieldev@gmail.com>
2024-12-10 19:42:25 -0600

    Se sube para consguir ultimos 3 - 2

--- getLast3Instruments.php
+++ getLast3Instruments.php
@@ -1,9 +1,5 @@
 <?php 
 
-include 'Vars/default.php';
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {die("Error al conectar a la base de datos: " . mysqli_connect_error());}
-
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     require_once 'default.php';
     $conn = mysqli_connect($servername, $username, $password, $dbname);
